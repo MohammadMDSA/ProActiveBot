9008392c9c1bce088f64192cb4446b5e7e0f7e02
Mohammad MDSA <samimd.77@gmail.com>
2017-10-30 13:35:59 +0330

    Added http object

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const restify = require('restify');
-const {Message, Prompts, UniversalBot, ChatConnector} = require('botbuilder');
+const { Message, Prompts, UniversalBot, ChatConnector } = require('botbuilder');
+const http = require('http');
 
-let server = restify.createServer();
-server.listen(3978, () => {
-	console.log(`${server.name} listening to %{server.url}`);
+const server = restify.createServer();
+server.listen(4000, () => {
+	console.log(`${server.name} listening to ${server.url}`);
 });
 
-let connector = ChatConnector({
-	appId: process.env.appId,
-	appPassword: process.env.appPassword
+const httpServer = http.createServer((req, res) => {
+	console.log('got something');
+	console.log(req);
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.write('sdfsdfdsf');
+	res.end();
+}).listen(8080);
+
+let connector = new ChatConnector({
+	appId: '',
+	appPassword: ''
 });
 
 server.post('/api/messages', connector.listen());
 
 let bot = new UniversalBot(connector, (session) => {
 	// Root dialog code goes here...
-})
+});
+
+
+
+
